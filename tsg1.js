11b4cf655ac13fea17dee8e9395bab0a73c460f1
ma125120 <1418205177@qq.com>
2019-07-03 10:36:21 +0800

    还在修改中

--- tsg1.js
+++ tsg1.js
@@ -1,5 +1,6 @@
 module.exports = {
+  // url: 'https://petstore.swagger.io/v2/swagger.json',
   url: 'http://api-alpha.heywoof.com/activity/v2/api-docs',
-  output: './src/test',
+  output: './src/api',
   staticPath: './request.ts',
 }
